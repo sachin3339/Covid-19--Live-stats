b8aba62e18bbd25dfb68a0607df62c90461ce0a1
Sachin <2017219@iiitdj.ac.in>
2020-03-25 23:37:47 +0530

    Country wise Data implemented

--- routes.js
+++ routes.js
@@ -1,7 +1,44 @@
-module.exports=(app)=>{
-    app.get('/',(req,res)=>{
-        res.render('home')
+const express = require('express');
+const app=express();
+const fetch = require("node-fetch");
+ var cases=0;
+module.exports=(app)=>
+{
+    app.get('/home',(req,res)=>{
+
+        fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+                "x-rapidapi-key": "5c7234d604mshc5a58d5c18d5ae3p178bcejsn46e9c8607b1a"
+            }
+        })
+        .then(response => response.json().then(data =>{
+            let countries_stat = data.countries_stat;
+            
+        cases=countries_stat[0].cases;
+        console.log(cases);
+        
+        }))
+            .catch(err => {
+                console.log(err);
+            });
+
+        res.render('map',{count:cases})
+    })
+   
+    
+   app.get('/',(req,res)=>{
+        res.render('table')
+    })
+
+
+    app.get('/chart',(req,res)=>{
+        res.render('chartvisual')
     })
 
 }
+
+
+
 
